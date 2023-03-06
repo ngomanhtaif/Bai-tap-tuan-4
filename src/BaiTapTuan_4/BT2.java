cdf0fce5cdcd8f6b0b059547a426bd49caeff65a
ngomanhtaif <126975941+ngomanhtaif@users.noreply.github.com>
2023-03-06 22:14:25 +0700

    week4

--- src/BaiTapTuan_4/BT2.java
+++ src/BaiTapTuan_4/BT2.java
@@ -1,12 +1,11 @@
-package BaiTapTuan_4;
 import java.util.Scanner;
 
 public class BT2{
 	public static void main(String[] args) {
 	int number;
 	System.out.println("Nhập số nguyên: ");
-	Scanner sc = new Scanner(System.in);
-	number = sc.nextInt();
+	Scanner scanner = new Scanner(System.in);
+	number = scanner.nextInt();
 	if(number % 2 == 0) {
 		System.out.println("Số chẵn");
 	}else {
